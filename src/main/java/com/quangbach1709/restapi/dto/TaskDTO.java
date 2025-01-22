8d2b3c422c06bbf846d785048ef2a827a045d5d1
quangbach1709 <bachh1124@edumail365.onmicrosoft.com>
2025-01-22 10:57:13 +0700

    fix validate in controller

--- src/main/java/com/quangbach1709/restapi/dto/TaskDTO.java
+++ src/main/java/com/quangbach1709/restapi/dto/TaskDTO.java
@@ -1,5 +1,6 @@
 package com.quangbach1709.restapi.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
@@ -12,9 +13,14 @@ public class TaskDTO {
     private String description;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
-    @Pattern(regexp = "^(High|Medium|Low)$", message = "Priority must be High, Medium or Low")
+    @NotBlank(message = "Priority cannot be blank")
+    @Pattern(regexp = "^(High|Medium|Low)$",
+            message = "Priority must be either 'High', 'Medium' or 'Low'")
     private String priority;
-    @Pattern(regexp = "^(New|In Progress|Completed|On Hold)$", message = "Status must be New, In Progress, Completed or On Hold")
+
+    @NotBlank(message = "Status cannot be blank")
+    @Pattern(regexp = "^(New|In Progress|Completed|On Hold)$",
+            message = "Status must be either 'New', 'In Progress', 'Completed' or 'On Hold'")
     private String status;
     private Long projectId;
     private String projectName;
